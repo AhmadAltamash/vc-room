6118a4255f2d7e3b334c06682a0769ea763c3a83
Altamash Ahmad <altamashahmad910@gmail.com>
2024-11-28 11:59:26 +0530

    commit

---
+++ src/components/MeetingTypeList.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import React, { useState } from 'react'
+import HomeCard from './HomeCard'
+import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
+
+const MeetingTypeList = () => {
+
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+
+    const router = useRouter()
+
+    const createMeeting = () => {
+        
+    }
+
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+
+        <HomeCard
+            img='/icons/add-meeting.svg'
+            title='New Meeting'
+            desc='Start an instant meeting'
+            className='bg-orange-1'
+            handleClick={() => setMeetingState('isInstantMeeting')}
+        />
+        <HomeCard
+            img='/icons/join-meeting.svg'
+            title='Join Meeting'
+            desc='via invitation link'
+            className='bg-blue-1'
+            handleClick={() => setMeetingState('isJoiningMeeting')}
+        />
+        <HomeCard
+            img='/icons/schedule.svg'
+            title='Schedule Meeting'
+            desc='Plan your meeting'
+            className='bg-purple-1'
+            handleClick={() => setMeetingState('isScheduleMeeting')}
+        />
+        <HomeCard
+            img='/icons/recordings.svg'
+            title='Recordings'
+            desc='Meeting Recordings'
+            className='bg-yellow-1'
+            handleClick={() => router.push('/recordings')}
+        />
+
+        <MeetingModal
+            className='text-center'
+            isOpen={meetingState === 'isInstantMeeting'}
+            onClose={() => setMeetingState(undefined)}
+            title='Start an Instant Meeting'
+            buttonText='Start Meeting'
+            handleClick={createMeeting}
+        />
+    </section>
+  )
+}
+
+export default MeetingTypeList
