5ba99f6d6944fc7a13828ce08f9822043660be7c
Altamash Ahmad <altamashahmad910@gmail.com>
2024-11-30 21:17:56 +0530

    commit

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -24,50 +24,17 @@ const MeetingSetup = () => {
     <div className='h-screen w-full flex flex-col items-center justify-center gap-3 text-white'>
       <h1 className='text-2xl font-bold'>Setup</h1>
       <VideoPreview/>
+
+      <div className='flex h-16 items-center justify-center gap-3'>
+        <label className='flex items-center justify-center gap-2 font-medium'>
+          <input type='checkbox' checked={isMicCamToggledOn}
+          onChange={(e)=> setIsMicCamToggledOn(e.target.checked)}/>
+          Join With Mic and Camera off
+        </label>
+      </div>
     </div>
   )
 }
 
 export default MeetingSetup
-// 'use client';
-// import { useCall, VideoPreview } from '@stream-io/video-react-sdk';
-// import React, { useEffect, useState } from 'react';
-
-// const MeetingSetup = () => {
-//   const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
-//   const call = useCall();
-
-//   useEffect(() => {
-//     if (isMicCamToggledOn) {
-//       call?.camera.enable()
-//         .then(() => console.log('Camera enabled'))
-//         .catch((error) => console.error('Failed to enable camera:', error));
-//       call?.microphone.enable()
-//         .then(() => console.log('Microphone enabled'))
-//         .catch((error) => console.error('Failed to enable microphone:', error));
-//     } else {
-//       call?.camera.disable()
-//         .then(() => console.log('Camera disabled'))
-//         .catch((error) => console.error('Failed to disable camera:', error));
-//       call?.microphone.disable()
-//         .then(() => console.log('Microphone disabled'))
-//         .catch((error) => console.error('Failed to disable microphone:', error));
-//     }
-//   }, [isMicCamToggledOn, call?.camera, call?.microphone]);
-
-//   return (
-//     <div className="h-screen w-full flex flex-col items-center justify-center gap-3 text-white">
-//       <h1 className="text-2xl font-bold">Setup</h1>
-//       <VideoPreview />
-//       <button
-//         className="btn"
-//         onClick={() => setIsMicCamToggledOn((prev) => !prev)}
-//       >
-//         {isMicCamToggledOn ? 'Disable Mic/Cam' : 'Enable Mic/Cam'}
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default MeetingSetup;
 
