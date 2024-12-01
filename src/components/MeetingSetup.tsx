2a3b5813bb98490015753b756a09982b3fe1b86b
Altamash Ahmad <altamashahmad910@gmail.com>
2024-12-01 12:29:20 +0530

    commit

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { useCall, VideoPreview } from '@stream-io/video-react-sdk'
+import { DeviceSettings, useCall, VideoPreview } from '@stream-io/video-react-sdk'
 import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button'
 
-const MeetingSetup = () => {
+const MeetingSetup = ({setIsSetupComplete}:{setIsSetupComplete: (value:boolean)=> void}) => {
   const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false)
   
   const call = useCall()
@@ -31,7 +32,14 @@ const MeetingSetup = () => {
           onChange={(e)=> setIsMicCamToggledOn(e.target.checked)}/>
           Join With Mic and Camera off
         </label>
+        <DeviceSettings/>
       </div>
+
+      <Button className="rounded-md bg-green-500 px-4 py-2.5" onClick={()=>{
+        call?.join()
+        setIsSetupComplete(true)
+        }}>Join Meeting</Button>
+
     </div>
   )
 }
